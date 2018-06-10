8452b363abdc7e76a9f75f7b97c182282a4802fc
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-10 17:57:01 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 def fizzbuzz(number)
   if (number %3 == 0 )
     puts "Fizz"
-  elsif (number / 5 == 0)
+  elsif (number % 5 == 0)
     puts "Buzz"
 ##  elsif (number/3 == 0 && number / 5 ==0)
 ##  else
