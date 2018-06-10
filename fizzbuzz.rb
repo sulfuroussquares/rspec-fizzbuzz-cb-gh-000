f6ee22268f78b0d2293320e05a4d452cec0bc3f0
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-06-10 17:58:48 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,9 @@
 def fizzbuzz(number)
   if (number %3 == 0 && number % 5 != 0)
     return "Fizz"
-  elsif (number % 5 == 0)
+  elsif (number % 5 == 0 && number %3 != 0 )
     return "Buzz"
-  elsif (number%3 == 0 && number % 5 ==0)
+  elsif (number%3 == 0 && number % 5 == 0)
     return "FizzBuzz"
 ##  else
 ##    return nil
